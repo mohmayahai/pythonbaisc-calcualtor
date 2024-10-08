be8613cca3516a33801820c82d15c009d1bed981
LOKENDRA <loki@LOKENDRAs-MacBook-Air.local>
2024-10-08 23:44:31 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,17 @@
+#shoping care program
+operator = input('Enter an operator( + , -, * ,/): ')
+num1 = int(input('Enter the 1st number'))
+num2 = int(input('enter the 2nd number'))
+
+
+if operator=='+':
+    print(num1+num2)
+elif operator=='-':
+    print(num1-num2)
+elif operator == '*':
+    print(num1*num2)
+else:
+    print(num1/+num2)
+
+
+
