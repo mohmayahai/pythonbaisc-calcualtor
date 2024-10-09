d43abfedb853014120d18b3a84a62ff40efef79c
LOKENDRA <loki@LOKENDRAs-MacBook-Air.local>
2024-10-09 10:39:57 +0530

    some changes fo the day2

---
+++ Day2.py
@@ -0,0 +1,25 @@
+#format specificier     ={value :falgs} fromat a value base on what flags are inserted
+
+price = 3.14159
+price2 = -987.65
+price3 = 13.24
+
+
+
+name = input('enter your name : ')
+
+#while name=='':
+   # print('did not enter the naneme')
+    #name = input('enter your name : ')
+
+
+print(f'your name {name}')
+#for loop can be used in range, sequence, string
+
+for x in range(0,10,2):
+    print(x)
+
+for x in range(5):
+    
+    print('*'*5)
+    
