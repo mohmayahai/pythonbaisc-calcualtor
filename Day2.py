1114466044b1ba7120f3e588a4b0f4fae5741400
LOKENDRA <loki@LOKENDRAs-MacBook-Air.local>
2024-10-10 19:02:46 +0530

    Full changes
    clear

--- Day2.py
+++ Day2.py
@@ -22,4 +22,4 @@ for x in range(0,10,2):
 for x in range(5):
     
     print('*'*5)
-    
+
--- intro.py
+++ intro.py
@@ -1,17 +1,9 @@
 #shoping care program
-operator = input('Enter an operator( + , -, * ,/): ')
-num1 = int(input('Enter the 1st number'))
-num2 = int(input('enter the 2nd number'))
+num=4
+print('positvie' if num>6 else 'Negative')
 
 
-if operator=='+':
-    print(num1+num2)
-elif operator=='-':
-    print(num1-num2)
-elif operator == '*':
-    print(num1*num2)
-else:
-    print(num1/+num2)
+
 
 
 
